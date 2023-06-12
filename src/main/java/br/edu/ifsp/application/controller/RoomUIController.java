2d56fa1597c37a1aabe1558b4bc29a0673362497
alinirv <alini.vicente@yahoo.com.br>
2023-06-11 22:58:01 -0300

    Adiciona validação de administrador para quarto

--- src/main/java/br/edu/ifsp/application/controller/RoomUIController.java
+++ src/main/java/br/edu/ifsp/application/controller/RoomUIController.java
@@ -2,8 +2,11 @@ package br.edu.ifsp.application.controller;
 
 import br.edu.ifsp.application.views.WindowLoader;
 import javafx.event.ActionEvent;
+import javafx.scene.control.Alert;
 
 import java.io.IOException;
+
+import static br.edu.ifsp.application.main.Main.autenticationUserUseCase;
 
 public class RoomUIController {
     public void returnRoomMenuUI(ActionEvent actionEvent) throws IOException {
@@ -11,14 +14,32 @@ public class RoomUIController {
     }
 
     public void createRoom(ActionEvent actionEvent) throws IOException {
-        WindowLoader.setRoot("RoomCreateUI");
+        try {
+            autenticationUserUseCase.verifyAdmin();
+            WindowLoader.setRoot("RoomCreateUI");
+        }catch(Exception e){
+            showAlert("Atenção!", e.getMessage(), Alert.AlertType.ERROR);
+        }
     }
 
     public void updateRoom(ActionEvent actionEvent) throws IOException {
-        WindowLoader.setRoot("RoomUpdateUI");
+        try {
+            autenticationUserUseCase.verifyAdmin();
+            WindowLoader.setRoot("RoomUpdateUI");
+        }catch(Exception e){
+            showAlert("Atenção!", e.getMessage(), Alert.AlertType.ERROR);
+        }
     }
 
     public void listRoom(ActionEvent actionEvent) throws IOException {
         WindowLoader.setRoot("RoomListUI");
+    }
+
+    private void showAlert(String title, String message, Alert.AlertType type){
+        Alert alert = new Alert(type);
+        alert.setTitle(title);
+        alert.setContentText(message);
+        alert.setHeaderText(null);
+        alert.showAndWait();
     }
 }
