5680647e1c8e879c785a0e0b85f0eb35e6374102
alinirv <alini.vicente@yahoo.com.br>
2023-06-11 22:35:05 -0300

    Adiciona validação de administrador para produto

--- src/main/java/br/edu/ifsp/application/controller/ProductUIController.java
+++ src/main/java/br/edu/ifsp/application/controller/ProductUIController.java
@@ -2,8 +2,11 @@ package br.edu.ifsp.application.controller;
 
 import br.edu.ifsp.application.views.WindowLoader;
 import javafx.event.ActionEvent;
+import javafx.scene.control.Alert;
 
 import java.io.IOException;
+
+import static br.edu.ifsp.application.main.Main.autenticationUserUseCase;
 
 public class ProductUIController {
 
@@ -13,14 +16,37 @@ public class ProductUIController {
     }
 
     public void createProduct(ActionEvent actionEvent) throws IOException {
-        WindowLoader.setRoot("ProductCreateUI");
+        try {
+            autenticationUserUseCase.verifyAdmin();
+            WindowLoader.setRoot("ProductCreateUI");
+        }catch(Exception e){
+            showAlert("Atenção!", e.getMessage(), Alert.AlertType.ERROR);
+        }
     }
 
     public void updateProduct(ActionEvent actionEvent) throws IOException {
-        WindowLoader.setRoot("ProductUpdateUI");
+        try {
+            autenticationUserUseCase.verifyAdmin();
+            WindowLoader.setRoot("ProductUpdateUI");
+        }catch(Exception e){
+            showAlert("Atenção!", e.getMessage(), Alert.AlertType.ERROR);
+        }
     }
 
     public void listProduct(ActionEvent actionEvent) throws IOException {
-        WindowLoader.setRoot("ProductListUI");
+        try {
+            autenticationUserUseCase.verifyAdmin();
+            WindowLoader.setRoot("ProductListUI");
+        }catch(Exception e){
+            showAlert("Atenção!", e.getMessage(), Alert.AlertType.ERROR);
+        }
+    }
+
+    private void showAlert(String title, String message, Alert.AlertType type){
+        Alert alert = new Alert(type);
+        alert.setTitle(title);
+        alert.setContentText(message);
+        alert.setHeaderText(null);
+        alert.showAndWait();
     }
 }
